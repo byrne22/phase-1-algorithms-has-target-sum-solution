70b970aebb7f84c274b99df9e5fc6b297f6720d8
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-21 22:55:19 -0400

    commit tree 820bc5e381feeecf9215e1e05cdb9377906aadd4

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    const nums = new Set();
+  for (const number of array) {
+    const targ = target - number;
+
+    if (nums.has(targ)) return true;
+
+    nums.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+    O(n^2)
+  It will take two iteration, the first through the entire array,
+  the next is look through the rest of the array as it sum up the elements. This is Quadratic Time.
 */
 
 /* 
   Add your pseudocode here
+   define a function that takes array and integer as arguments
+    if the sum of a pair in array is same as interger:
+    return true
+    else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+    define a function, hasTargetSum, that receives two arguments, an array and interger,
+  the function should return true if the sum of two numbers in the array equals the interger
 */
 
 // You can run `node index.js` to view these console logs
